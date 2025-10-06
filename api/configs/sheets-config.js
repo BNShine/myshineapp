20538bb823c85ddcd0b969d8172e4ccbe9ef5b43
BNShine <alanqms2@gmail.com>
2025-10-06 01:34:36 -0300

    Create sheets-config.js

---
+++ api/configs/sheets-config.js
@@ -0,0 +1,10 @@
+// api/configs/sheets-config.js
+
+export const SHEET_NAME_APPOINTMENTS = 'Datatest';
+export const SHEET_NAME_EMPLOYEES = 'Employees';
+export const SHEET_NAME_FRANCHISES = 'Regions';
+export const SHEET_NAME_USERS = 'Users';
+export const SHEET_NAME_ROLES = 'Roles';
+export const SHEET_NAME_TECHNICIANS = 'Technicians';
+export const SHEET_NAME_TECH_COVERAGE = 'TechCoverageData';
+export const SHEET_NAME_AVAILABILITY = 'TechnicianAvailability';
